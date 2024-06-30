a351de5406b44f1b25f6b806a3acbfc6f821b4bb
Kris Kurzawa <kurzawa1@yahoo.com>
2024-06-30 18:57:35 -0400

    updated margin

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <>
       {/* medium */}
-      <main className='crop-circle-container hidden md:flex lg:hidden flex-col items-center w-full h-full'>
+      {/* <main className='crop-circle-container hidden md:flex lg:hidden flex-col items-center w-full h-full'>
         <article className='circle circle1-md flex items-center justify-center'>Text</article>
         <article className='rectangle rectangle1-md flex items-center justify-center'>Text</article>
         <article className='circle circle2-md flex items-center justify-center'>Text</article>
@@ -23,20 +23,20 @@ const App = () => {
           <article className='line line5-md'>Text</article>
           <article className='line line6-md'>Text</article>
         </section>
-      </main>
+      </main> */}
       {/* large */}
-      <main className='crop-circle-container flex flex-col items-center w-full h-full'>
+      <main className='crop-circle-container flex flex-col items-center w-full h-fit'>
         {/* <article className='circle circle1 flex items-center justify-center'>Text</article>
         <article className='rectangle rectangle1 flex items-center justify-center'>Text</article> */}
         <article className='circle circle2 flex items-center justify-center mb-40'>Text</article>
         <article className='rectangle rectangle2 flex items-center justify-center'>Text</article>
         <article className='circle circle3 flex items-center justify-center mb-96'>Text</article>
         <article className='rectangle rectangle3 flex items-center justify-center'>Text</article>
-        <section className='circle5-container flex justify-center w-full h-full'>
-          {/* <article className='line line2'>Text</article> */}
-          <article className='circle circle5 flex items-center justify-center'>Text</article>
-          {/* <article className='line line3'>Text</article> */}
-        </section>
+        {/* <section className='circle5-container flex justify-center w-full h-full'> */}
+        {/* <article className='line line2'>Text</article> */}
+        <article className='circle circle5 flex items-center justify-center'>Text</article>
+        {/* <article className='line line3'>Text</article> */}
+        {/* </section> */}
         {/* <section className='line-container flex'>
           <article className='line line4'>Text</article>
           <article className='line line5'>Text</article>
